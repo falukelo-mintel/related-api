2317843fcb9e6abcaee1603ffe009f12b694e80b
falukelo-mintel <106736300+falukelo-mintel@users.noreply.github.com>
2022-06-13 18:20:31 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 import numpy as np
-from utils import related_similarity_content
+from utils import related_similarity_content_tfidf
 data_model = pd.read_csv('data/df_associations.csv')
-df_article = pd.read_csv('data/list_article.csv')
+df_recomendation = pd.read_csv('recommended-20.csv')
 
 class Item(BaseModel):
     url: str
@@ -14,26 +14,32 @@ class Item(BaseModel):
 app = FastAPI()
 
 @app.post("/get_related/")
-async def create_item(item: Item):
+async def get_related(item: Item):
     input_url = item.url
     df_query = data_model.loc[data_model['antecedents'] == input_url]
     df_final = df_query.sort_values('confidence', ascending=False)[:3]
     results_product = []
     consequents = df_final['consequents'].values.tolist()
     if len(consequents) == 0:
-        results_product = related_similarity_content(data_model, input_url, df_article)
+        lst = related_similarity_content_tfidf(df_associations, input_url, df_recomendation)
+        lst_sim = [r for r in lst if r not in consequents][:3]
+        results_product = consequents + lst_sim
     elif len(consequents) == 1:
-        lst = related_similarity_content(data_model, input_url, df_article)
+        lst = related_similarity_content_tfidf(df_associations, input_url, df_recomendation)
         lst_sim = [r for r in lst if r not in consequents][:2]
         results_product = consequents + lst_sim
     elif len(consequents) == 2:
-        results_product = consequents
-        results_product.append('/'.join(consequents[0].split('/')[:-1]))
+        lst = related_similarity_content_tfidf(df_associations, input_url, df_recomendation)
+        lst_sim = [r for r in lst if r not in consequents][:1]
+        results_product = consequents + lst_sim
     else:
         results_product = consequents
     result = {"related_product": results_product}
     return JSONResponse(content=result)
 
+@app.post("/update_related/")
+async def update_related(item: Item):
+    
 @app.get("/")
 async def test():
     return "Hello Mintel"
