7bb1f8765e05df175580c3bda1ee0be79b4c215b
falukelo-mintel <somyost@mintel.tech>
2022-06-14 09:26:37 +0000

    recommendation v:0.0.2

--- app/main.py
+++ app/main.py
@@ -9,12 +9,14 @@ import numpy as np
 # from sklearn.feature_extraction.text import TfidfTransformer
 from urllib.parse import quote, unquote
 import ast
+from google.cloud import firestore
 
 cossim_matrix = pd.read_csv('data/cossim_matrix.csv')
 recommended = pd.read_csv('data/recommended.csv')
 
 class Item(BaseModel):
     url: str
+    cx_cookie: str
 
 app = FastAPI()
 
@@ -22,17 +24,30 @@ app = FastAPI()
 async def create_item(item: Item):
     input_url = item.url
     input_url = unquote(input_url)
+    cx_cookie = item.cx_cookie
+    db = firestore.Client()
+    doc_ref = db.collection(u'Organizes/pJoo5lLhhAbbofIfYdLz/objects/activities/data')
+    query = doc_ref.where(u'cx_cookie', u'==', cx_cookie).get()
+    history = [q.to_dict()['cx_web_url_fullpath'].split('?')[0] for q in query]
+    history = list(set(history))
+    
     url = '/'.join(input_url.split('/')[4:])
     description = recommended.loc[recommended['DocumentUrlPath'].str.contains(url)]
     cosine_score = cossim_matrix.values.tolist()
     print(type(cosine_score))
     
     result = {}
+    count = 0
     list_recommend = ast.literal_eval(description['recommend'].values[0])
-    for i, idx in enumerate(list_recommend):
+    for idx in list_recommend:
         score = cosine_score[description.index[0]][idx]
-        result[i] = {'url': recommended.iloc[recommended.index == idx]['DocumentUrlPath'].values[0],
-                     'similar_score': 1 - score}
+        text = df_text.iloc[df_text.index == idx]['DocumentUrlPath'].values[0]
+        if any(text not in his for his in history):
+            result[count] = {'url': text,
+                         'similar_score': 1 - score}
+            count += 1
+        if count == 3:
+            break
     return JSONResponse(content=result)
 
 @app.get("/")
