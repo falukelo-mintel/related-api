8396335b20086090efa40f16df6fff542aa052fb
falukelo-mintel <somyost@mintel.tech>
2022-06-22 18:26:39 +0000

    training API v:0.0.2

--- app/.ipynb_checkpoints/main-checkpoint.py
+++ app/.ipynb_checkpoints/main-checkpoint.py
@@ -85,8 +85,19 @@ async def train_related(item: Item_rel):
     df_associations['confidence'] = df_associations['frequency']/df_associations['frequency_antecedent']
     df_associations['antecedents'] = df_associations['antecedents'].str.replace('https://www.krungsri.com/bank', 'https://www.krungsri.com')
     df_associations['antecedents'] = df_associations['antecedents'].str.replace('.html', '')
-    df_associations.to_csv('data/df_associations.csv', index=None)
-    subprocess.call(['sh', './gitprocess_rel.sh'])
+    bucket = client.get_bucket(BUCKET_NAME)
+    blob_source_name = 'krungsri/model/rel/main/data/df_associations.csv'
+    source_blob = bucket.blob(blob_source_name)
+    if source_blob.exists():
+        blob_dest_name = f'krungsri/model/rel/tmp/data/df_associations-{source_blob.time_created}.csv'
+
+        new_blob = bucket.copy_blob(
+            source_blob, bucket, blob_dest_name)
+        source_blob.delete()
+    else:
+        print(f'Source blob [{blob_source_name}] not exists.')
+    df_associations.to_csv(f'gs://{BUCKET_NAME}/krungsri/model/rel/main/data/df_associations.csv', index=None)
+    # subprocess.call(['sh', './gitprocess_rel.sh'])
     return 'success'
 
 @app.post("/train_recommendation/")
