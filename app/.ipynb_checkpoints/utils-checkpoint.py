0cfa74208b496b2620d15ce0846f637a48204929
falukelo-mintel <somyost@mintel.tech>
2022-07-14 08:09:38 +0000

    update return

---
+++ app/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import ast
+
+def overlapping_sublists(l, n, overlap=1, start=0):          
+    while start <= len(l) - n:
+        yield l[start:start+n]
+        start += n - overlap
+        
+def get_cateandsub(content_url, df_article):
+    content_name = content_url.split('/')[-1]
+    df_tmp = df_article.loc[df_article["DocumentUrlPath"].str.contains(content_name, na=False)]
+    node_alias_path = df_tmp['NodeAliasPath'].values.tolist()[0]
+    url_split = node_alias_path.split('/')
+    category = url_split[1]
+    sub_category = url_split[2]
+    return category, sub_category
+
+
+def related_similarity_content(df, content_url, df_article):
+    category, sub_category = get_cateandsub(content_url, df_article)
+    print(category, sub_category)
+    df_tmp = df.loc[df["antecedents"].str.contains("/{}/{}".format(category, sub_category), na=False)]
+    if len(df_tmp) == 0:
+        df_tmp = df.loc[df["antecedents"].str.contains("/{}/".format(category), na=False)]
+    if len(df_tmp) == 1:
+        result = df_tmp["consequents"].values.tolist()
+        df_tmp = df.loc[df["antecedents"].str.contains("/{}/".format(category), na=False)]
+        result.append(list(set(df_tmp["consequents"].values.tolist()))[:2])
+    if len(df_tmp) == 2:
+        result = df_tmp["consequents"].values.tolist()
+        df_tmp = df.loc[df["antecedents"].str.contains("/{}/".format(category), na=False)]
+        result.append(df_tmp["consequents"].values.tolist()[:1])
+    else:
+        result = list(set(df_tmp["consequents"].values.tolist()))[:3]
+    return result
+
+def related_similarity_content_tfidf(df, content_url, df_recomendation):
+    content_name = content_url.split('/')[-1]
+    df_rec = df_recomendation.loc[df_recomendation["DocumentUrlPath"].str.contains(content_name, na=False)]
+    list_recommend = ast.literal_eval(df_rec['recommend'].values[0])
+    sublist = overlapping_sublists(list_recommend, 4)
+    ss = []
+    for s in sublist:
+        ss += s
+        df_search = df_recomendation.iloc[ss]
+        df_tmp = df.loc[df["antecedents"].str.contains('|'.join(df_search['DocumentUrlPath'].values), na=False)]
+        df_tmp = df_tmp.drop_duplicates(subset=['antecedents'])
+        df_tmp = df_tmp.drop_duplicates(subset=['consequents'])
+        if len(df_tmp) > 3:
+            print(ss)
+            break
+    df_tmp = df_tmp.sort_values(by=['frequency_antecedent', 'confidence'], ascending=False)
+    return df_tmp['consequents'].values
