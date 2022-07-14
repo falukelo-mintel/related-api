cf3e18292346e0932f992e384fd5552ba041c3e6
falukelo-mintel <somyost@mintel.tech>
2022-07-14 08:30:36 +0000

    update return

--- app/main.py
+++ app/main.py
@@ -24,35 +24,26 @@ async def get_related(item: Item):
     df_final = df_query.sort_values('confidence', ascending=False)
     results_product = []
     consequents = df_final['consequents'].values.tolist()
-    if len(consequents) == 0:
-        lst = related_similarity_content_tfidf(data_model, input_url, df_recomendation)
-        lst_sim = [r for r in lst if r not in consequents]
-        results_product = consequents + lst_sim
-    elif len(consequents) == 1:
-        lst = related_similarity_content_tfidf(data_model, input_url, df_recomendation)
-        lst_sim = [r for r in lst if r not in consequents]
-        results_product = consequents + lst_sim
-    elif len(consequents) == 2:
-        lst = related_similarity_content_tfidf(data_model, input_url, df_recomendation)
-        lst_sim = [r for r in lst if r not in consequents]
-        results_product = consequents + lst_sim
-    else:
-        results_product = consequents
-        
-    result = {"related_product": []}
     
-    for r in results_product:
-        url = r.replace('/th/', '/').replace('/en/', '/')
-        query = doc_ref.where(u'url', u'==', url).get()
-        if query:
-            q = query[0].to_dict()
-            del q['createdBy']
-            del q['lastModified']
-            del q['createdDate']
-            del q['modifiedBy']
-            result['related_product'].append(q)
+    result = {"related_product": []}
+    while True:
+        for cons in consequents:
+            url = cons.replace('/th/', '/').replace('/en/', '/')
+            query = doc_ref.where(u'url', u'==', url).get()
+            if query:
+                q = query[0].to_dict()
+                del q['createdBy']
+                del q['lastModified']
+                del q['createdDate']
+                del q['modifiedBy']
+                result['related_product'].append(q)
+            if result['related_product'].__len__() == 3:
+                break
         if result['related_product'].__len__() == 3:
             break
+        else:
+            lst = related_similarity_content_tfidf(data_model, input_url, df_recomendation)
+            consequents = [r for r in lst if r not in result['related_product']]
         
     return JSONResponse(content=result)
     
