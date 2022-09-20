2f2cc6a788e30f956197f3d29cca89ce7db09a83
falukelo-mintel <somyost@mintel.tech>
2022-09-20 07:58:01 +0000

    v2.0.0

--- app/.ipynb_checkpoints/main-checkpoint.py
+++ app/.ipynb_checkpoints/main-checkpoint.py
@@ -7,8 +7,9 @@ import numpy as np
 from utils import related_similarity_content_tfidf
 from google.cloud import firestore
 from fastapi.middleware.cors import CORSMiddleware
-data_model = pd.read_csv('data/df_associations.csv')
-df_recomendation = pd.read_csv('gs://connect-x-production.appspot.com/Organizes/pJoo5lLhhAbbofIfYdLz/AI/model/recommended.csv')
+from urllib.parse import quote, unquote
+# data_model = pd.read_csv('data/df_associations.csv')
+df_recomendation = pd.read_csv('gs://connect-x-production.appspot.com/Organizes/pJoo5lLhhAbbofIfYdLz/AI/model/recprod_master.csv')
 
 # class Item(BaseModel):
 #     url: str
@@ -26,39 +27,57 @@ app.add_middleware(
 @app.get("/get_related/")
 async def get_related(url: str, cookie: str):
     db = firestore.Client()
-    doc_ref = db.collection(u'Organizes/pJoo5lLhhAbbofIfYdLz/objects/krungsriProduct/data')
-    input_url = url
-    df_query = data_model.loc[data_model['antecedents'] == input_url]
-    df_final = df_query.sort_values('confidence', ascending=False)
-    results_product = []
-    consequents = df_final['consequents'].values.tolist()
+    prod_ref = db.collection(u'Organizes/pJoo5lLhhAbbofIfYdLz/objects/krungsriProduct/data')
+    art_ref = db.collection(u'Organizes/pJoo5lLhhAbbofIfYdLz/objects/articleContent/data')
     
-    result = {"related_product": []}
-    count = 0
-    while True:
-        for cons in consequents:
-            url = cons.replace('/th/', '/').replace('/en/', '/')
-            # print(url)
-            query = doc_ref.where(u'url', u'==', url).get()
-            if query:
-                q = query[0].to_dict()
-                del q['createdBy']
-                del q['lastModified']
-                del q['createdDate']
-                del q['modifiedBy']
-                if q not in result['related_product']:
-                    result['related_product'].append(q)
-            if result['related_product'].__len__() == 3:
-                break
-        if result['related_product'].__len__() == 3:
-            break
-        else:
-            lst = related_similarity_content_tfidf(data_model, input_url, df_recomendation)
-            df_rec = df_recomendation.loc[df_recomendation.index == int(lst[count])]
-            df_query = data_model.loc[data_model['antecedents'] == f'{df_rec.link.values[0]}']
-            df_final = df_query.sort_values('confidence', ascending=False)
-            consequents = df_final['consequents'].values.tolist()
-            count += 1
+    input_url = url
+    input_url = unquote(input_url)
+    text = input_url.split('/')[-1]
+    df_result = df_recomendation.loc[df_recomendation.link.str.contains(text)]
+    if des_list.__len__() > 0:
+        des_list = df_result.values.tolist()[0][2:]
+        result = {"related_products": []}
+        query = prod_ref.where(u'description', u'in', des_list).get()
+        for qry in query:
+            q = qry.to_dict()
+            del q['createdBy']
+            del q['lastModified']
+            del q['createdDate']
+            del q['modifiedBy']
+            result['related_products'].append(q)
+    else:
+        result = {'status': 400}
+#     df_query = data_model.loc[data_model['antecedents'] == input_url]
+#     df_final = df_query.sort_values('confidence', ascending=False)
+#     results_product = []
+#     consequents = df_final['consequents'].values.tolist()
+    
+#     result = {"related_product": []}
+#     count = 0
+#     while True:
+#         for cons in consequents:
+#             url = cons.replace('/th/', '/').replace('/en/', '/')
+#             # print(url)
+#             query = doc_ref.where(u'url', u'==', url).get()
+#             if query:
+#                 q = query[0].to_dict()
+#                 del q['createdBy']
+#                 del q['lastModified']
+#                 del q['createdDate']
+#                 del q['modifiedBy']
+#                 if q not in result['related_product']:
+#                     result['related_product'].append(q)
+#             if result['related_product'].__len__() == 3:
+#                 break
+#         if result['related_product'].__len__() == 3:
+#             break
+#         else:
+#             lst = related_similarity_content_tfidf(data_model, input_url, df_recomendation)
+#             df_rec = df_recomendation.loc[df_recomendation.index == int(lst[count])]
+#             df_query = data_model.loc[data_model['antecedents'] == f'{df_rec.link.values[0]}']
+#             df_final = df_query.sort_values('confidence', ascending=False)
+#             consequents = df_final['consequents'].values.tolist()
+#             count += 1
         #     consequents = [r for r in lst if r not in result['related_product']]
         # if count == 10:
         #     break
